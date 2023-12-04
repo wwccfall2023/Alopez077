c89985f5318f4c821129713b1327b3b2594f13ae
Alopez077 <122071367+Alopez077@users.noreply.github.com>
2023-12-03 19:48:05 -0800

    Update answers.sql

--- week7/answers.sql
+++ week7/answers.sql
@@ -85,7 +85,7 @@ LEFT JOIN items i ON inv.item_id = i.item_id OR eq.item_id = i.item_id;
 DROP FUNCTION IF EXISTS armor_total;
 DELIMITER //
 
-CREATE OR REPLACE FUNCTION armor_total(character_id INT UNSIGNED) RETURNS INT
+CREATE FUNCTION armor_total(character_id INT UNSIGNED) RETURNS INT
 DETERMINISTIC
 BEGIN
     DECLARE total_armor INT;
